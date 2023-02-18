0392c0ea8c7aef8580ce16d0de4e508c5a51cce3
Flavio Rizzoglio <flavio.rizzoglio@mail.polimi.it>
2023-02-18 15:30:30 +0100

    chore: :lipstick: added custom lightblue

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        myLightBlue: "#30c9e8",
+      },
+    },
   },
   plugins: [require("@tailwindcss/line-clamp")],
 };
