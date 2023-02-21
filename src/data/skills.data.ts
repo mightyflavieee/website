16909f68c08d2b5dfe9c704515938680f62c82b3
Flavio Rizzoglio <flavio.rizzoglio@mail.polimi.it>
2023-02-21 17:52:08 +0100

    feat: :sparkles: added some skills

---
+++ src/data/skills.data.ts
@@ -0,0 +1,26 @@
+export function getSkills(): string[] {
+  const skills: string[] = [
+    "Teamwork",
+    "Communication",
+    "Problem solving",
+    "Linux",
+    "Git",
+    "CI/CD",
+    "SQL & NoSQL",
+    "Typescript",
+    "Python",
+    "Java",
+    "C",
+    "Angular",
+    "VueJS",
+    "NodeJS",
+    "HTML & CSS",
+    "Tailwind",
+    "Firebase",
+    "Apache Kafka",
+    "Apache Spark",
+    "Agile Development",
+  ];
+
+  return skills;
+}
